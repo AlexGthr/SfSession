e0108e4ed9846bb9e128861978320289a6dc2bea
Alex Gthr <agauthier5@outlook.fr>
2024-04-20 20:03:23 +0200

    ajout darkmode

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const btnToggleTheme = document.querySelector(".btn-toggle");
+
+    if (!localStorage.getItem("theme")) {
+        document.body.classList.add("dark-theme");
+    }
+  
+    // Vérifier que l'élément .btn-toggle existe avant d'attacher l'événement
+    const currentTheme = localStorage.getItem("theme");
+    if (currentTheme == "dark") {
+        document.body.classList.add("dark-theme");
+    } 
+    
+    if (btnToggleTheme) {
+      btnToggleTheme.addEventListener("click", function () {
+        document.body.classList.toggle("dark-theme");
+  
+        let theme = "light";
+        if (document.body.classList.contains("dark-theme")) {
+          theme = "dark";
+        }
+        localStorage.setItem("theme", theme);
+        })
+    }
+});
+
 $(document).ready(function() {
     $('.cardModule').click(function() {
         $(this).toggleClass('active'); // Ajoute ou supprime la classe 'active' à la cardModule cliquée
@@ -11,3 +37,4 @@ $(document).ready(function() {
         $('.dashbord_liste').slideToggle("slow");
     })
 });
+
